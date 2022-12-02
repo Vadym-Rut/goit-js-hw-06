060aac8499ab40f3f3412e8759b88613caa21569
Vadym <110600784+Vadym-Rut@users.noreply.github.com>
2022-12-02 15:20:00 +0200

    Add files via upload

---
+++ js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const list = document.querySelector('#ingredients')
+
+for (let i = 0; i < ingredients.length; i++) {
+	const listItem = document.createElement('li')
+	listItem.textContent = ingredients[i]
+	listItem.classList.add('item')
+	list.append(listItem)
+}
